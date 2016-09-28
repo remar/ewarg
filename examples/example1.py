e3eb1478b34793fa51c159423b5dc5d4a534a84b
Andreas Remar <andreas.remar@gmail.com>
2016-09-28 15:20:52 +0200

    Implemented set_tilesize, load_tileset, and set_tile Removed set_background_color

--- examples/example1.py
+++ examples/example1.py
@@ -2,11 +2,22 @@ import sys
 
 sys.path.append("../src")
 
-import ewarg, sdl2, sdl2.ext, random
+import ewarg, sdl2, sdl2.ext
+
+sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
 
 ewarg = ewarg.ewarg()
 
-ewarg.init(320, 240)
+ewarg.init(320, 256)
+ewarg.set_tilesize(32, 32)
+ewarg.load_tileset("block", "data/block1.bmp")
+
+for y in range(4):
+    for x in range(5):
+        ewarg.set_tile(x*2, y*2, "block", 3, 0)
+        ewarg.set_tile(x*2+1, y*2, "block", 4, 0)
+        ewarg.set_tile(x*2, y*2+1, "block", 5, 0)
+        ewarg.set_tile(x*2+1, y*2+1, "block", 6, 0)
 
 running = True
 
@@ -15,8 +26,6 @@ while running:
         if event.type == sdl2.SDL_QUIT:
             running = False
             break
-
-    ewarg.set_background_color(random.randint(0, 255),
-                               random.randint(0, 255),
-                               random.randint(0, 255))
     ewarg.redraw()
+
+sdl2.SDL_Quit()
--- src/ewarg.py
+++ src/ewarg.py
@@ -1,12 +1,13 @@
-import sdl2
+import sdl2, math
 
 class ewarg(object):
     def __init__(self):
-        # TODO: set up data structures for tilesets, sprites, and sprite
-        # instances
-        pass
+        self.tiles = []
+        self.tilesets = {}
 
     def init(self, width, height):
+        self.width = width
+        self.height = height
         self.window = sdl2.SDL_CreateWindow(b"ewarg",
                                             sdl2.SDL_WINDOWPOS_CENTERED,
                                             sdl2.SDL_WINDOWPOS_CENTERED,
@@ -19,7 +20,54 @@ class ewarg(object):
 
     def redraw(self):
         sdl2.SDL_RenderClear(self.renderer)
+        for l in self.tiles:
+            for tile in l:
+                tile.draw(self.renderer)
         sdl2.SDL_RenderPresent(self.renderer)
 
-    def set_background_color(self, r, g, b):
-        sdl2.SDL_SetRenderDrawColor(self.renderer, r, g, b, 255)
+    def set_tilesize(self, width, height):
+        self.tile_width = width
+        self.tile_height = height
+
+        self._init_tiles()
+
+    def load_tileset(self, name, filename):
+        image = sdl2.SDL_LoadBMP(bytes(filename, 'ascii'))
+        self.tilesets[name] = sdl2.SDL_CreateTextureFromSurface(self.renderer,
+                                                                image)
+        sdl2.SDL_FreeSurface(image)
+
+
+    def set_tile(self, x, y, tileset, tile_x, tile_y):
+        self.tiles[x][y].set(self.tilesets[tileset], tile_x, tile_y)
+
+    def _init_tiles(self):
+        tiles_per_row = int(math.ceil(float(self.width) / self.tile_width))
+        tiles_per_col = int(math.ceil(float(self.height) / self.tile_height))
+        self.tiles = []
+        self.blank = sdl2.SDL_CreateTexture(self.renderer,
+                                            sdl2.SDL_PIXELFORMAT_ARGB8888,
+                                            sdl2.SDL_TEXTUREACCESS_STATIC,
+                                            self.tile_width,
+                                            self.tile_height)
+        for x in range(tiles_per_row):
+            self.tiles.append([])
+            for y in range(tiles_per_col):
+                t = Tile(x, y, self.tile_width, self.tile_height)
+                t.set(self.blank, 0, 0)
+                self.tiles[x].append(t)
+
+class Tile(object):
+    def __init__(self, x, y, width, height):
+        self.texture = None
+        self.dest = sdl2.SDL_Rect(x*width, y*height, width, height)
+        self.width = width
+        self.height = height
+
+    def set(self, texture, tile_x, tile_y):
+        self.texture = texture
+        self.src = sdl2.SDL_Rect(tile_x*self.width, tile_y*self.height,
+                                 self.width, self.height)
+
+    def draw(self, renderer):
+        sdl2.SDL_RenderCopy(renderer, self.texture, self.src, self.dest)
