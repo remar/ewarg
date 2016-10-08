ceed75acd643d2d038065064a30ef60b0d3f6299
Andreas Remar <andreas.remar@gmail.com>
2016-10-08 20:39:30 +0200

    Extended example3

--- examples/example3.py
+++ examples/example3.py
@@ -2,54 +2,71 @@ import sys
 
 sys.path.append("../src")
 
-import ewarg, sdl2, sdl2.ext, random
+import ewarg, sdl2, sdl2.ext, random, math
 
 sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
 
 ewarg = ewarg.ewarg()
 
-ewarg.init(320, 256)
+WIDTH = 32*10
+HEIGHT = 32*8
+
+ewarg.init(WIDTH, HEIGHT)
 ewarg.set_tilesize(32, 32)
 ewarg.load_tileset("block", "data/block1.bmp")
 ewarg.load_sprite("data/fuzz.json")
 
-for y in range(1, 7):
-    ewarg.set_tile(0, y, "block", 2, 0)
-    ewarg.set_tile(9, y, "block", 2, 0)
+row = math.ceil(WIDTH / 32)
+col = math.ceil(HEIGHT / 32)
 
-for x in range(1, 9):
+for y in range(1, col - 1):
+    ewarg.set_tile(0, y, "block", 2, 0)
+    ewarg.set_tile(row - 1, y, "block", 2, 0)
+
+for x in range(1, row - 1):
     ewarg.set_tile(x, 0, "block", 1, 0)
-    ewarg.set_tile(x, 7, "block", 1, 0)
+    ewarg.set_tile(x, col - 1, "block", 1, 0)
 
 ewarg.set_tile(0, 0, "block", 3, 0)
-ewarg.set_tile(9, 0, "block", 4, 0)
-ewarg.set_tile(0, 7, "block", 5, 0)
-ewarg.set_tile(9, 7, "block", 6, 0)
+ewarg.set_tile(row - 1, 0, "block", 4, 0)
+ewarg.set_tile(0, col - 1, "block", 5, 0)
+ewarg.set_tile(row - 1, col - 1, "block", 6, 0)
 
 class Fuzz(object):
     def __init__(self, ewarg):
         self.ewarg = ewarg
         self.sprite_id = ewarg.create_sprite_instance("fuzz")
-        self.x = random.randint(33, 320-32-24-1)
-        self.y = random.randint(33, 256-32-24-1)
+        self.x = random.randint(33, WIDTH-32-24-1)
+        self.y = random.randint(33, HEIGHT-32-24-1)
         self.dx = -1 if random.randint(0, 1) == 0 else +1
         self.dy = -1 if random.randint(0, 1) == 0 else +1
-        ewarg.set_animation(self.sprite_id, "roll right")
+        self.rolling_left = True if random.randint(0, 1) == 0 else False
+        self.update_animation()
         ewarg.move_sprite_abs(self.sprite_id, self.x, self.y)
 
     def move(self):
         self.x += self.dx
         self.y += self.dy
 
-        if self.x == 32 or self.x == 320-32-24:
+        if self.x == 32 or self.x == WIDTH-32-24:
             self.dx = -self.dx
-        if self.y == 32 or self.y == 256-32-24:
+            self.toggle_direction()
+        if self.y == 32 or self.y == HEIGHT-32-24:
             self.dy = -self.dy
+            self.toggle_direction()
 
         ewarg.move_sprite_abs(self.sprite_id, self.x, self.y)
 
     def remove(self):
         self.ewarg.remove_sprite_instance(self.sprite_id)
+
+    def toggle_direction(self):
+        self.rolling_left = not self.rolling_left
+        self.update_animation()
+
+    def update_animation(self):
+        ewarg.set_animation(self.sprite_id,
+                            "roll left" if self.rolling_left else "roll right")
 
 fuzzes = []
 for i in range(10):
